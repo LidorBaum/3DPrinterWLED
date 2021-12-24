7a61c604f6e3fb97f747433d792129c9ae7f85c0
Lidor Baum <86967825+LidorBaum@users.noreply.github.com>
2021-12-24 02:53:00 +0200

    Readme (#2)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * added config file
    
    * added config file
    
    * fixed bugs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const http = require("http").createServer(app);
+const http = require("http").createServer(app);
 const { initiateLEDS, states } = require("./service");
 const { host, MQTTport } = require("./config");
 
