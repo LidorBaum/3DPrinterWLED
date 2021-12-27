5d51d8ae65bc6d325b1611660c41306ac9aaef54
Lidor Baum <Lidor5500@gmail.com>
2021-12-27 18:43:32 +0200

    added api key

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const host = "192.168.1.11";
+const host = "192.168.1.12";
 const MQTTport = "1883";
 const LEDS = 52;
 const ROWS = 2;
-const OCTOPRINT = "http://192.168.1.11";
+const OCTOPRINT = "http://192.168.1.12";
 const APIKEY = "F4D745711FAA491AB5F0A652DB3F0EDB"
 const WLED = "http://192.168.1.38";
 
@@ -13,4 +13,5 @@ module.exports = {
   ROWS,
   OCTOPRINT,
   WLED,
+  APIKEY
 };
